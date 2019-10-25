9dcdd837122cce935a1c0f32f28269cc5c35112b
Sujan Maharjan <zexcu008@gmail.com>
2019-10-25 10:25:36 +0545

    Added Countries to the ListView

--- app/src/main/java/com/sujanmaharjan008/listview/MainActivity.java
+++ app/src/main/java/com/sujanmaharjan008/listview/MainActivity.java
@@ -3,12 +3,26 @@ package com.sujanmaharjan008.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+     public  static final String countries[] = {"Nepal","Russia","China","Japan","US"};
+     private ListView lsvCount;
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lsvCount = findViewById(R.id.lsvCount);
+
+         ArrayAdapter countryAdapter = new ArrayAdapter<>(
+                 this,
+                 android.R.layout.simple_list_item_1,
+                 countries
+         );
+
+         lsvCount.setAdapter(countryAdapter);
     }
 }
